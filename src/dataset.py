a21a143c7be0ffcbce2106ca7be82f1221ba8c68
mehtameet <mehtameet007@gmail.com>
2014-04-09 00:58:10 -0700

    get_datasets done ane returning lists

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,6 @@
 import subprocess
+import requests
+import json
 
 class datasets:
     
@@ -18,4 +20,23 @@ class datasets:
         output, error = process.communicate()       
         
         return output
+    
+    def get_datasets(self,dataset_name):
+        r = requests.get('http://54.186.225.72:50070/webhdfs/v1/data/'+dataset_name+'/data?op=LISTSTATUS')
+        #print r.json()
+#         for v in r.json():
+#             print v
+#         print r.json()['FileStatuses']['FileStatus']
+        name_list=[]
+        for v in r.json()['FileStatuses']['FileStatus']:
+            if(v['type']=="FILE"):
+                print v['pathSuffix']
+                name_list.append(v['pathSuffix'])
+        return name_list
+#         bashCommand='curl -i "http://54.186.225.72:50070/webhdfs/v1/data/'+dataset_name+'/data?op=LISTSTATUS"'
+#         process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+#         output, error = process.communicate()
+#         print ("output is "+output)
+#         print (error)
+#         return output
     
