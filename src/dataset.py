3891a1ed72f8080be922fab10ca57af810930af8
mehtameet <mehtameet007@gmail.com>
2014-04-08 23:20:48 -0700

    initial file dump to hdfs commit

---
+++ src/dataset.py
@@ -0,0 +1,21 @@
+import subprocess
+
+class datasets:
+    
+    def upload(self,dataset_name):
+        bashCommand='curl -i -X PUT "http://54.186.225.72:50070/webhdfs/v1/data/'+dataset_name+'?user.name=ubuntu&op=MKDIRS"'
+        process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        print ("output is "+output)
+        print (error)
+        
+        bashCommand='$HADOOP_PREFIX/bin/hadoop fs -copyFromLocal ~/code/data/* /data/'+dataset_name+'/*'
+        process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+
+        bashCommand='rm ~/code/data/*'
+        process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()       
+        
+        return output
+    
---
+++ src/test.py
@@ -0,0 +1,7 @@
+from dataset import datasets
+
+datasetObj=datasets()
+
+str1=datasetObj.upload("dataset1")
+
+print (str1)
