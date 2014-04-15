33f95fa15264369d0fe40188a63ceff0533a24e1
mehtameet <mehtameet007@gmail.com>
2014-04-14 23:34:50 -0700

    changing trainlogistic function

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,8 +1,11 @@
 import subprocess
 import requests
 import json
+from dataset import datasets
 
 class algorithms:
+    
+    
     
     def trainlogistic(self,dataset_name,predictors,target):
         str_predictor=""
@@ -10,7 +13,9 @@ class algorithms:
         for i in predictors:
             count+=1
             str_predictor+=i+" "
-        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data/  --output ~/code/bdmaas/data/"+dataset_name+"/model  --target "+target+"   --categories "+str(count)+"  --predictors "+str_predictor+"   --types word  --features 20   --passes 100 --rate 50"
+        datasetObj=datasets()
+        dataset_files=datasetObj.get_datasetsFirstFilename(dataset_name)
+        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data/"+dataset_files[0]+"  --output ~/code/bdmaas/data/"+dataset_name+"/model  --target "+target+"   --categories "+str(count)+"  --predictors "+str_predictor+"   --types word  --features 20   --passes 100 --rate 50"
         process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output, error = process.communicate()
         print ("output is "+output)
@@ -18,7 +23,9 @@ class algorithms:
         return "trainlogistic done"
     
     def runlogistic(self,dataset_name):
-        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data/  --model ~/code/bdmaas/data/"+dataset_name+"/model --auc --confusion"
+        datasetObj=datasets()
+        dataset_files=datasetObj.get_datasetsFirstFilename(dataset_name)
+        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data/"+dataset_files[0]+"  --model ~/code/bdmaas/data/"+dataset_name+"/model --auc --confusion"
         process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output, error = process.communicate()
         print ("output is "+output)
