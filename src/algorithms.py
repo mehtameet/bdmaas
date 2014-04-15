c0f611e8f7bbcd6f08230f7f9a084704f0204910
mehtameet <mehtameet007@gmail.com>
2014-04-14 23:27:09 -0700

    changing trainlogistic function

--- src/algorithms.py
+++ src/algorithms.py
@@ -10,7 +10,7 @@ class algorithms:
         for i in predictors:
             count+=1
             str_predictor+=i+" "
-        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data  --output ~/code/bdmaas/data/"+dataset_name+"/model  --target "+target+"   --categories "+str(count)+"  --predictors "+str_predictor+"   --types word  --features 20   --passes 100 --rate 50"
+        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data/  --output ~/code/bdmaas/data/"+dataset_name+"/model  --target "+target+"   --categories "+str(count)+"  --predictors "+str_predictor+"   --types word  --features 20   --passes 100 --rate 50"
         process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output, error = process.communicate()
         print ("output is "+output)
@@ -18,7 +18,7 @@ class algorithms:
         return "trainlogistic done"
     
     def runlogistic(self,dataset_name):
-        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data  --model ~/code/bdmaas/data/"+dataset_name+"/model --auc --confusion"
+        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data/  --model ~/code/bdmaas/data/"+dataset_name+"/model --auc --confusion"
         process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output, error = process.communicate()
         print ("output is "+output)
