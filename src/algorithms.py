2104708f44a864121456255c8952afef77d0e9c6
mehtameet <mehtameet007@gmail.com>
2014-04-14 23:11:04 -0700

    adding initial trainlogistic function

---
+++ src/algorithms.py
@@ -0,0 +1,31 @@
+import subprocess
+import requests
+import json
+
+class algorithms:
+    
+    def trainlogistic(self,dataset_name,predictors,target):
+        str_predictor=""
+        count=0
+        for i in predictors:
+            count+=1
+            str_predictor+=i+" "
+        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data  --output ~/code/bdmaas/data/"+dataset_name+"/model  --target "+target+"   --categories "+count+"  --predictors "+str_predictor+"   --types word  --features 20   --passes 100 --rate 50"
+        process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        print ("output is "+output)
+        print (error)
+        return "trainlogistic done"
+    
+    def runlogistic(self,dataset_name,predictors,target):
+        str_predictor=""
+        count=0
+        for i in predictors:
+            count+=1
+            str_predictor+=i+" "
+        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data  --model ~/code/bdmaas/data/"+dataset_name+"/model --auc --confusion"
+        process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        print ("output is "+output)
+        print (error)
+        return "runlogistic done"
--- src/test.py
+++ src/test.py
@@ -4,5 +4,17 @@ datasetObj=datasets()
 
 #str1=datasetObj.upload("dataset1")
 #jsondata=datasetObj.get_datasets("dataset1")
-jsondata=datasetObj.get_columns("dataset1")
-print (jsondata)
+# jsondata=datasetObj.get_datasets()
+#jsondata=datasetObj.get_columns("dataset7")
+# print (type(jsondata))
+# innerhtml=""
+# for i in jsondata:
+#     innerhtml+=i
+#     
+# print innerhtml
+#print jsondata
+
+from algorithms import algorithms
+algorithmObj=algorithms()
+
+algorithmObj.trainlogistic("contract1", ["seller_company_name","customer_company_name"], "point_of_delivery_specific_location")
