fc70d3ccc19739a697bebf155362614f4628f70c
Kuntal0711 <kuntalshah07@gmail.com>
2014-04-16 02:41:15 -0700

    testKuntal add

--- src/testKuntal.py
+++ src/testKuntal.py
@@ -5,7 +5,7 @@ datasetObj=datasets()
 from algorithms import algorithms
 
 algorithmObj=algorithms()
-algorithmObj.testrandomforest("dataset10")
 #algorithmObj.testrandomforest("dataset10")
+algorithmObj.trainrandomforest("dataset10")
 #algorithmObj.runrandomforest("dataset10")
 
