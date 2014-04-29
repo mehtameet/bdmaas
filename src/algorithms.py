27f870034e7cba29f75616ba618e7cdf2260446f
mehtameet <mehtameet007@gmail.com>
2014-04-28 18:17:17 -0700

    trainlogistic updating categories

--- src/algorithms.py
+++ src/algorithms.py
@@ -18,7 +18,7 @@ class algorithms:
             str_predictor+=i+" "
         #datasetObj=datasets()
         dataset_files=datasetObj.get_datasetsFirstFilename(dataset_name)
-        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data/train/"+dataset_files[0]+"  --output ~/code/bdmaas/data/"+dataset_name+"/model  --target "+target+"   --categories "+str(count)+"  --predictors "+str_predictor+"   --types word  --features 20   --passes 100 --rate 50"
+        bashCommand="$MAHOUT_HOME/bin/mahout trainlogistic  --input ~/code/bdmaas/data/"+dataset_name+"/data/train/"+dataset_files[0]+"  --output ~/code/bdmaas/data/"+dataset_name+"/model  --target "+target+"   --categories 2  --predictors "+str_predictor+"   --types word  --features 20   --passes 100 --rate 50"
         process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output, error = process.communicate()
         print ("output is "+output)
